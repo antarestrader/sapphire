c2669c4b13a1bb24313b1a0df20937bb42397e4d
John F. Miller <jfmiller28@yahoo.com>
2013-12-06 12:05:11 -0800

    Improved Show instance for Var

--- Var.hs
+++ Var.hs
@@ -1,7 +1,14 @@
 module Var where
 
-data Var = Var {name :: String, scope :: Scope} | Self deriving Show 
-  -- TODO: Better Show for Var
+data Var = Var {name :: String, scope :: Scope} | Self
+
+instance Show Var where
+  show Self = "self"
+  show Var {name = n, scope = []} = n
+  show Var {name = n, scope = xs} = scopeToString "" xs ++ n
+    where 
+      scopeToString str [] = str
+      scopeToString str (x:xs) = scopeToString (str++x++"::") xs
 
 type Op = String
 
