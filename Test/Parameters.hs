70a87e6e1281a3562362c583ca375fdaf2cbe835
John F. Miller <jfmiller28@yahoo.com>
2017-03-27 21:55:47 -0800

    A few tests for Parameters

--- Test/Parameters.hs
+++ Test/Parameters.hs
@@ -1,29 +1,30 @@
-
+--Copyright 2017 by John F. Miller
 {-# LANGUAGE  GeneralizedNewtypeDeriving, MultiParamTypeClasses #-}
 module Test.Parameters 
   ( module Eval.Parameters
   , module Test.Parameters
   , module Name
   , module Object
+  , quickCheck
   )
 where
 
 import Control.Monad.State
 import Control.Monad.Except
+import Data.String
 import qualified Data.Map.Strict as M
+import Test.QuickCheck
 
 import Name
 import String
 import Object hiding (State)
 import Eval.Parameters
 import Scope
+import qualified Array as A
 
-newtype S a= S{ uns :: State (Namespace Object) a} 
-  deriving (Functor, Applicative, Monad, MonadState (Namespace Object))
-
-instance MonadError Object S where
-  throwError s = error $ show s
-  catchError = undefined
+newtype S a= S{ uns :: StateT (Namespace Object) (Except Object) a} 
+  deriving (Functor, Applicative, Monad,
+            MonadState (Namespace Object), MonadError Object)
 
 instance Scope S where
   readVar Local n = do
@@ -41,7 +42,38 @@ instance Scope S where
   spawn = undefined
   presidenceTable = undefined
 
-run :: S a -> Namespace Object
-run x = execState (uns x) (M.empty) 
+run :: S a -> Either Object (Namespace Object)
+run x = runExcept $ execStateT (uns x) (M.empty)
 
 runMatch p xs = run $ match p xs
+
+instance Arbitrary Object where
+  arbitrary = Prim <$> arbitrary
+
+instance Arbitrary Primitive where
+   arbitrary = oneof [
+       VInt <$> arbitrary
+     , VString . fromString <$> arbitrary
+     , VFloat <$> arbitrary
+     , VAtom <$> arbitrary
+     , VArray . A.fromList <$> arbitrary
+     ]
+
+prop_Empty xs =  --Empty only matches the empty list
+   if (length xs) == 0
+    then
+      case r of
+        Right _ -> True
+        Left _  -> False
+    else
+      case r of
+        Right _ -> False
+        Left  _ -> True
+  where r = runMatch Empty xs
+
+prop_Asterisk xs = -- an Astrisk should match everything
+  let r = runMatch (Asterisk "foo") xs in
+    case r of 
+      Left _ -> False
+      Right m ->  maybe False (\_ -> True) (M.lookup "foo" m)
+
